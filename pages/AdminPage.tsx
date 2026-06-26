a705dbdb132d56e6d26c2a680dc94c65fb350692
Tuco2006 <mateussmcruz@icloud.com>
2026-06-26 17:51:27 -0300

    changing bg color

--- pages/AdminPage.tsx
+++ pages/AdminPage.tsx
@@ -47,8 +47,8 @@ const StockMovementModal: React.FC<{
     );
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-xl shadow-xl w-full max-w-md">
+    <div className="fixed inset-0 bg-black bg-opacity-20 flex justify-center items-center z-50">
+      <div className="bg-purple-600 p-6 rounded-xl shadow-xl w-full max-w-md">
         <h2 className="text-xl font-bold mb-4 text-blue-800">
           Movimentacao de Estoque
         </h2>
